72667ec1393354444a33da705f343e36ecabdb14
vmuthyala <vmuthyala@GBWK-INA034036Z.galaxy.lan>
2020-03-02 18:54:12 +0530

    eureks server

---
+++ src/main/java/com/boot/MicroservicesEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MicroservicesEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesEurekaServerApplication.class, args);
+	}
+
+}
